cb369792693497bde03e65225dd5b79d88416a43
MarDak24 <maxim.matjerow@gmail.com>
2023-07-03 23:39:56 +0300

    +

--- src/js/dark-light-mode.js
+++ src/js/dark-light-mode.js
@@ -1,33 +1,5 @@
-// const themeSwitcher = document.querySelector('.switcher');
-// const slider = document.querySelector('.slider');
-
-// themeSwitcher.addEventListener('click', function () {
-//   toggleTheme();
-// });
-
-// function toggleTheme() {
-//   const isDarkMode = themeSwitcher.classList.toggle('dark-theme');
-//   const themeName = isDarkMode ? 'dark-theme' : 'light-theme';
-//   applyTheme(themeName);
-//   saveTheme(themeName);
-// }
-
-// function applyTheme(themeName) {
-//   const themeStylesheet = document.querySelector('[title="theme"]');
-// themeStylesheet.setAttribute('href', `./css/${themeName}.css`);
-// }
-
-// function saveTheme(themeName) {
-//   localStorage.setItem('theme', themeName);
-// }
-
-// const activeTheme = localStorage.getItem('theme');
-// if (activeTheme) {
-//   themeSwitcher.classList.add(activeTheme);
-//   applyTheme(activeTheme);
-// }
-// 2222222222222222
 const themeSwitcher = document.querySelector('.switcher');
+const slider = document.querySelector('.slider');
 
 themeSwitcher.addEventListener('click', function () {
   toggleTheme();
@@ -35,14 +7,14 @@ themeSwitcher.addEventListener('click', function () {
 
 function toggleTheme() {
   const isDarkMode = themeSwitcher.classList.toggle('dark-theme');
-  const themeName = isDarkMode ? 'dark' : 'light';
+  const themeName = isDarkMode ? 'dark-theme' : 'light-theme';
   applyTheme(themeName);
   saveTheme(themeName);
 }
 
 function applyTheme(themeName) {
   const themeStylesheet = document.querySelector('[title="theme"]');
-  themeStylesheet.setAttribute('href', `./css/${themeName}.css`);
+  themeStylesheet.setAttribute('href', `css/${themeName}.css`);
 }
 
 function saveTheme(themeName) {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './js/api';
 import './js/support-ukraine';
 import './js/categorues';
-import './js/shopping';
 import './js/bestSeller';
 import './js/menu';
 import './js/dark-light-mode';
