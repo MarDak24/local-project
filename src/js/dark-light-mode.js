2642ce11a48939e6700474864961b1fe8c2a332b
MarDak24 <maxim.matjerow@gmail.com>
2023-07-03 23:14:48 +0300

    +

--- src/js/dark-light-mode.js
+++ src/js/dark-light-mode.js
@@ -27,30 +27,30 @@
 //   applyTheme(activeTheme);
 // }
 // 2222222222222222
-// const themeSwitcher = document.querySelector('.switcher');
+const themeSwitcher = document.querySelector('.switcher');
 
-// themeSwitcher.addEventListener('click', function () {
-//   toggleTheme();
-// });
+themeSwitcher.addEventListener('click', function () {
+  toggleTheme();
+});
 
-// function toggleTheme() {
-//   const isDarkMode = themeSwitcher.classList.toggle('dark-theme');
-//   const themeName = isDarkMode ? 'dark' : 'light';
-//   applyTheme(themeName);
-//   saveTheme(themeName);
-// }
+function toggleTheme() {
+  const isDarkMode = themeSwitcher.classList.toggle('dark-theme');
+  const themeName = isDarkMode ? 'dark' : 'light';
+  applyTheme(themeName);
+  saveTheme(themeName);
+}
 
-// function applyTheme(themeName) {
-//   const themeStylesheet = document.querySelector('[title="theme"]');
-//   themeStylesheet.setAttribute('href', `./css/${themeName}.css`);
-// }
+function applyTheme(themeName) {
+  const themeStylesheet = document.querySelector('[title="theme"]');
+  themeStylesheet.setAttribute('href', `./css/${themeName}.css`);
+}
 
-// function saveTheme(themeName) {
-//   localStorage.setItem('theme', themeName);
-// }
+function saveTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+}
 
-// const activeTheme = localStorage.getItem('theme');
-// if (activeTheme) {
-//   themeSwitcher.classList.add(activeTheme);
-//   applyTheme(activeTheme);
-// }
+const activeTheme = localStorage.getItem('theme');
+if (activeTheme) {
+  themeSwitcher.classList.add(activeTheme);
+  applyTheme(activeTheme);
+}
