1e80f11cef26f5b8d7691fd25de3bc79d873b9bc
MarDak24 <maxim.matjerow@gmail.com>
2023-07-05 23:50:38 +0300

    +

--- src/js/dark-light-mode.js
+++ src/js/dark-light-mode.js
@@ -1,20 +1,60 @@
-const themeSwitcher = document.querySelector('.switcher');
-const slider = document.querySelector('.slider');
+// const themeSwitcher = document.querySelector('.switcher');
+// const bodyElement = document.querySelector('body');
+// const otherElements = document.querySelectorAll(
+//   '.slider',
+//   '.general-header',
+//   '.header-logo-icon',
+//   '.gallery-heading',
+//   '.see-more',
+//   '.gallery-books-title',
+//   '.shopping-title',
+//   '.choosenbook-title',
+//   '.choosenbook-descr',
+//   '.icon-open',
+//   '.categories-link'
+// );
 
-themeSwitcher.addEventListener('click', function () {
-  toggleTheme();
-});
+// themeSwitcher.addEventListener('click', toggleTheme);
+
+// function toggleTheme() {
+//   const isDarkMode = bodyElement.classList.toggle('dark');
+//   const themeName = isDarkMode ? 'dark' : 'light';
+//   saveTheme(themeName);
+// }
+
+// function saveTheme(themeName) {
+//   localStorage.setItem('theme', themeName);
+// }
+
+// const activeTheme = localStorage.getItem('theme');
+// if (activeTheme === 'dark') {
+//   bodyElement.classList.add('dark');
+// } else {
+//   bodyElement.classList.remove('dark');
+// }
+
+// otherElements.forEach(function (element) {
+//   if (activeTheme === 'dark') {
+//     element.classList.add('dark');
+//   } else {
+//     element.classList.remove('dark');
+//   }
+// });
+
+const themeSwitcher = document.querySelector('.switcher');
+const bodyElement = document.querySelector('body');
+const otherElements = document.querySelectorAll(
+  '.slider, .general-header, .header-logo-icon, .gallery-heading, .see-more, .gallery-books-title, .shopping-title, .choosenbook-title, .choosenbook-descr, .icon-open, .categories-link'
+);
+
+themeSwitcher.addEventListener('click', toggleTheme);
 
 function toggleTheme() {
-  const isDarkMode = themeSwitcher.classList.toggle('dark-theme');
-  const themeName = isDarkMode ? 'dark-theme' : 'light-theme';
-  applyTheme(themeName);
-  saveTheme(themeName);
-}
-
-function applyTheme(themeName) {
-  const themeStylesheet = document.querySelector('[title="theme"]');
-  themeStylesheet.setAttribute('href', `css/${themeName}.css`);
+  bodyElement.classList.toggle('dark');
+  saveTheme(bodyElement.classList.contains('dark') ? 'dark' : 'light');
+  otherElements.forEach(function (element) {
+    element.classList.toggle('dark');
+  });
 }
 
 function saveTheme(themeName) {
@@ -22,7 +62,14 @@ function saveTheme(themeName) {
 }
 
 const activeTheme = localStorage.getItem('theme');
-if (activeTheme) {
-  themeSwitcher.classList.add(activeTheme);
-  applyTheme(activeTheme);
+if (activeTheme === 'dark') {
+  bodyElement.classList.add('dark');
+  otherElements.forEach(function (element) {
+    element.classList.add('dark');
+  });
+} else {
+  bodyElement.classList.remove('dark');
+  otherElements.forEach(function (element) {
+    element.classList.remove('dark');
+  });
 }
